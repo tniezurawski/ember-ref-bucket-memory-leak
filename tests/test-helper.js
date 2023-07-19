bd05f17f9cc7f4b710491c11867b9d043f94a8a8
Tomasz Nieżurawski <tommaqs@gmail.com>
2023-07-19 17:50:49 +0200

    Add Ember

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-ref-bucket-memory-leak/app';
+import config from 'ember-ref-bucket-memory-leak/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
